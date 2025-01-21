fcaac7e0a940261e3d4db0184f17147d3cd400ca
Hugo <ducky4tools@gmail.com>
2025-01-21 23:07:13 +0100

    Cleaning jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,3 @@
-// module.exports = {
-//     preset: 'ts-jest',
-//     testEnvironment: 'node',
-//     globals: {
-//       'ts-jest': {
-//         isolatedModules: true,
-//       },
-//     },
-//   };
-  
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
