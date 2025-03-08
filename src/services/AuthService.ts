79bf076b7fbb93349a22549d25016736764c364b
Hugo <ducky4tools@gmail.com>
2025-03-08 16:04:57 +0100

    Fix

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -4,7 +4,7 @@ import AccountModel from '../models/AccountModel';
 import { ErrorEnum } from '../enums/errorEnum';
 
 const SECRET_KEY = process.env.JWT_SECRET || 'defaultsecret';
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1h';
+const JWT_EXPIRES_IN = '1h';
 
 class AuthService {
   public static async register(firstName: string, lastName: string, email: string, password: string, phoneNumber: string): Promise<string> {
