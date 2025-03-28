0d77890397490715c72e56b72cd103f67358b354
Corentin Talour <corentintalour@gmail.com>
2025-03-28 15:19:34 +0100

    test: skip existing tests for getMyMissions in listMissions.test.ts

--- src/tests/mission/listMissions.test.ts
+++ src/tests/mission/listMissions.test.ts
@@ -1,17 +1,12 @@
 describe("GET /api/mission (getMyMissions)", () => {
 
-    test("Renvoie les missions assignées avec filtres et tri", async () => {
-    });
+    test.skip("Renvoie les missions assignées avec filtres et tri", async () => {});
 
-    test("Retourne les missions triées par missionType", async () => {
-    });
+    test.skip("Retourne les missions triées par missionType", async () => {});
 
-    test("Renvoie une liste vide si aucun résultat dans la plage de dates", async () => {
-    });
+    test.skip("Renvoie une liste vide si aucun résultat dans la plage de dates", async () => {});
 
-    test("Renvoie toutes les missions sans filtre", async () => {
-    });
+    test.skip("Renvoie toutes les missions sans filtre", async () => {});
 
-    test("Renvoie 401 si l'utilisateur n'est pas authentifié", async () => {
-    });
+    test.skip("Renvoie 401 si l'utilisateur n'est pas authentifié", async () => {});
 });
