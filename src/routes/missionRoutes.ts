9f942e910e38e7e0ab5843ae675a1620e781e4ef
Corentin Talour <corentintalour@gmail.com>
2025-04-13 17:53:26 +0200

    feat: add message retrieval by mission ID and improve error handling

--- src/routes/missionRoutes.ts
+++ src/routes/missionRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import upload from "../services/UploadService";
-import { createMission, updateMission } from "../controllers/MissionController";
+import {createMission, getMessagesByMissionId, updateMission} from "../controllers/MissionController";
 import { auth } from "../middleware/authMiddleware";
 
 const router = Router();
@@ -65,4 +65,7 @@ const router = Router();
 router.post("/", auth, upload, createMission);
 
 router.put("/:id", auth, updateMission)
+
+router.get("/:idMission", auth, getMessagesByMissionId)
+
 module.exports = router;
