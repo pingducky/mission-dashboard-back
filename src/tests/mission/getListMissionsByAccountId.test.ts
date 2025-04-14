bd5b1a8b94aae35f5f6d5c527ce53b277e105f54
Corentin Talour <corentintalour@gmail.com>
2025-04-14 20:57:34 +0200

    feat: add tests for getListMissionsByAccountId endpoint with various filters

---
+++ src/tests/mission/getListMissionsByAccountId.test.ts
@@ -0,0 +1,22 @@
+describe.skip('GET /mission/:id', () => {
+    test.skip("Doit retourner toutes les missions associées à l'utilisateur", async () => {
+    });
+
+    test.skip("Doit filtrer les missions à partir d'une date (from)", async () => {
+    });
+
+    test.skip("Doit filtrer les missions jusqu'à une date (to)", async () => {
+    });
+
+    test.skip("Doit filtrer les missions entre deux dates (from + to)", async () => {
+    });
+
+    test.skip("Doit filtrer les missions par type de mission (filterByType)", async () => {
+    });
+
+    test.skip("Doit retourner une erreur si l'utilisateur n'existe pas", async () => {
+    });
+
+    test.skip("Doit retourner une erreur si l'identifiant est invalide", async () => {
+    });
+});
