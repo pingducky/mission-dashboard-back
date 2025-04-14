d7f64511228d1768925483e91dcab05e709f709e
Corentin Talour <corentintalour@gmail.com>
2025-04-14 21:25:25 +0200

    test: Ajout test

---
+++ src/tests/mission/getMessagesByMissionId.test.ts
@@ -0,0 +1,13 @@
+describe.skip('GET /mission/:idMission/messages', () => {
+    test.skip("Doit retourner tous les messages d'une mission", async () => {
+    });
+
+    test.skip("Doit retourner les messages triés du plus récent au plus ancien", async () => {
+    });
+
+    test.skip("Doit retourner une erreur si l'identifiant de mission est invalide", async () => {
+    });
+
+    test.skip("Doit retourner une erreur si la mission n'existe pas", async () => {
+    });
+});
