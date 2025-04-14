7d7d4980ed9f7b1cf762adf08a6d4e7b7516bcae
Corentin Talour <corentintalour@gmail.com>
2025-04-14 21:12:51 +0200

    test: ajout de tests pour l'ajout de messages à une mission

---
+++ src/tests/mission/addMessageToMission.test.ts
@@ -0,0 +1,19 @@
+describe.skip('POST /mission/:idMission/message', () => {
+    test.skip("Doit ajouter un message à une mission", async () => {
+    });
+
+    test.skip("Doit retourner une erreur si le message est manquant", async () => {
+    });
+
+    test.skip("Doit retourner une erreur si l'idAccount est manquant", async () => {
+    });
+
+    test.skip("Doit retourner une erreur si idMission est invalide", async () => {
+    });
+
+    test.skip("Doit retourner une erreur si la mission n'existe pas", async () => {
+    });
+
+    test.skip("Doit retourner une erreur si l'utilisateur n'existe pas", async () => {
+    });
+});
