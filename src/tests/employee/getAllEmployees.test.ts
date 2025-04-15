f47740366c5a18d7b647c361ad1f9d3e79c32bec
Elise-44 <bouardelise.perso@gmail.com>
2025-04-15 22:01:02 +0200

    ajout test

--- src/tests/employee/getAllEmployees.test.ts
+++ src/tests/employee/getAllEmployees.test.ts
@@ -7,7 +7,7 @@ let authToken: string;
 beforeAll(async () => {
     await resetDatabase();
 
-    const userResponse = await request(app)
+    const user1 = await request(app)
         .post("/api/auth/register")
         .send({
             firstName: "John",
@@ -17,7 +17,27 @@ beforeAll(async () => {
             phoneNumber: "1234567890",
         });
 
-    authToken = userResponse.body.token;
+    const user2 = await request(app)
+        .post("/api/auth/register")
+        .send({
+            firstName: "Jane",
+            lastName: "Smith",
+            email: "jane.smith@example.com",
+            password: "password123",
+            phoneNumber: "0987654321",
+        });
+
+    // Authentification avec le premier user 1
+    authToken = user1.body.token;
+
+    // Modification manuel de l'état du user 2
+    await request(app)
+        .put(`/api/employee/${user2.body.id}`)
+        .set("Authorization", `Bearer ${authToken}`)
+        .send({
+            isEnabled: false,
+            isOnline: true,
+        });
 });
 
 afterAll(async () => {
@@ -32,6 +52,39 @@ describe("Employee API", () => {
 
         expect(response.status).toBe(200);
         expect(Array.isArray(response.body)).toBe(true);
+    });
+
+    test("Doit récupérer uniquement les employés actifs", async () => {
+        const res = await request(app)
+            .get("/api/employee?status=active")
+            .set("Authorization", `Bearer ${authToken}`);
+
+        expect(res.status).toBe(200);
+        res.body.forEach((emp: any) => {
+            expect(emp.isEnabled).toBe(true);
+        });
+    });
+
+    test("Doit récupérer uniquement les employés inactifs", async () => {
+        const res = await request(app)
+            .get("/api/employee?status=inactive")
+            .set("Authorization", `Bearer ${authToken}`);
+
+        expect(res.status).toBe(200);
+        res.body.forEach((emp: any) => {
+            expect(emp.isEnabled).toBe(false);
+        });
+    });
+
+    test("Doit récupérer uniquement les employés en ligne", async () => {
+        const res = await request(app)
+            .get("/api/employee?status=online")
+            .set("Authorization", `Bearer ${authToken}`);
+
+        expect(res.status).toBe(200);
+        res.body.forEach((emp: any) => {
+            expect(emp.isOnline).toBe(true);
+        });
     });
 
     test("Doit empêcher un utilisateur non authentifié d'accéder à la liste des employés", async () => {
