abff4ea3e00db7d7b496a63aaa160052d3e9eada
Hugo <ducky4tools@gmail.com>
2025-04-23 21:45:33 +0200

    Ajouts des tests vide de la création d'employée.

---
+++ src/tests/employee/createEmployee.test.ts
@@ -0,0 +1,24 @@
+describe('Création d\'un employé', () => {
+    test.skip('Doit retourner une erreur si un champ requis est manquant', async () => {
+    });
+  
+    test.skip('Doit retourner une erreur si l\'email est invalide', async () => {
+    });
+  
+    test.skip('Doit retourner une erreur si l\'email est déjà utilisé', async () => {
+    });
+  
+    test.skip('Doit retourner une erreur si un rôle fourni n\'existe pas', async () => {
+    });
+  
+    test.skip('Doit créer un employé même si aucun rôle n\'est fourni', async () => {
+    });
+  
+    test.skip('Doit hasher le mot de passe généré avant création', async () => {
+    });
+  
+    test.skip('Doit retourner un statut 201 avec les infos de l\'employé', async () => {
+    });
+
+  });
+  
