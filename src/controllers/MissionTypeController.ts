13baf04c2acf5ea038a8317699d97135c08bc93e
Corentin Talour <corentintalour@gmail.com>
2025-05-01 11:36:25 +0200

    fix: simplify getAllMissionsTypes by removing unnecessary attributes in query

--- src/controllers/MissionTypeController.ts
+++ src/controllers/MissionTypeController.ts
@@ -4,9 +4,7 @@ import {handleHttpError} from "../services/ErrorService";
 
 export const getAllMissionsTypes = async (req: Request, res: Response): Promise<void> => {
     try {
-        const missionTypes = await MissionTypeModel.findAll({
-            attributes: ['id', 'shortLibel', 'longLibel']
-        });
+        const missionTypes = await MissionTypeModel.findAll();
 
         res.status(200).json({ missionTypes });
     } catch (error) {
